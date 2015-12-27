778a13b0547a944a8e48b4314e9725792a4e9c58
Mark Tse <mark.tse@neverendingqs.com>
2015-12-26 19:17:00 -0500

    Added simple functionality and manually calls a logger.

---
+++ postsharp-log4net/Foo.cs
@@ -0,0 +1,24 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace postsharp_log4net
+{
+    internal sealed class Foo
+    {
+        private static readonly ILog _logger = LogManager.GetLogger(typeof(Foo));
+
+        internal void DoSomething()
+        {
+            _logger.Info("Doing something");
+        }
+
+        internal void DoSomethingElse()
+        {
+            _logger.Info("Doing something else");
+        }
+    }
+}
---
+++ postsharp-log4net/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace postsharp_log4net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Foo f = new Foo();
+            f.DoSomething();
+            f.DoSomethingElse();
+
+            Console.In.Read();
+        }
+    }
+}
